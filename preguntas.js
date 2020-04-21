b58aced0a5895eee45e886b2545d56a973b88c0d
Camila <camila.ardila.ca@gmail.com>
2020-04-21 18:32:15 -0500

    js

---
+++ preguntas.js
@@ -0,0 +1,21 @@
+const correcta = document.getElementById('correcta');
+const incorrecta = document.getElementById('incorrecta');
+const resultado = document.getElementById('total');
+
+
+
+addEventListener('click', calcular);
+
+function calcular () { 
+
+
+if () { 
+correcta.innerHTML = 'Bien'
+incorrecta.innerHTML = 'Mal'
+  
+}
+
+
+}
+
+
