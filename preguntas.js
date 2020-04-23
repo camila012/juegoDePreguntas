5e5ef7532cebd785bf9fcccf7086a76bd2860e1b
Camila <camila.ardila.ca@gmail.com>
2020-04-23 16:58:34 -0500

    intentos

--- preguntas.js
+++ preguntas.js
@@ -1,21 +1,27 @@
-const correcta = document.getElementById('correcta');
-const incorrecta = document.getElementById('incorrecta');
-const resultado = document.getElementById('total');
-
+const correcta = document.getElementById("correcta");
+const incorrecta = document.getElementById("incorrecta");
+const resultado = document.getElementById("total");
+const preguntas = document.querySelector(".preguntas")
 
 
 addEventListener('click', calcular);
 
+
+
 function calcular () { 
-
-
-if () { 
-correcta.innerHTML = 'Bien'
-incorrecta.innerHTML = 'Mal'
+  for ( i = 1; i < preguntas; i++) {
+    const verificar = preguntas[i];
+    
   
-}
+  if (verificar ) { 
+    correcta.dataset.index ||  incorrecta.dataset.index
+  
+  console.log(calcular)
+ 
 
 
 }
+}
 
+}
 
